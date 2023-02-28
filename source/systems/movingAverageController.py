ec8d7153260c00bb214369e1c921e5fa226b384f
Maksim Isakov <maksim.isakov@mytona.com>
2023-02-28 22:23:26 +0400

    fix averages

--- source/systems/movingAverageController.py
+++ source/systems/movingAverageController.py
@@ -8,22 +8,28 @@ class MovingAverageController:
         self.__maxAverageSize = movingAverage.getMaxAverageSize(self.__averages)
 
     def __calculateSMA(self, amount:int):
-        if len(self.__closes) >= amount:
-            return sum(self.__closes[-amount:]) / amount
-        return None
+        if len(self.__closes) < amount:
+            return None
+        return sum(self.__closes[-amount:]) / amount
 
-    def __calculateEMA(self, lastValue, amount:int):
+    def __calculateEMA(self, type:movingAverage.MovingAverageType, amount:int):
+        lastValue = self.__lastValues.get(type)
         if lastValue is None:
-            return self.__closes[-1]
-        alpha = 2 / (amount + 1)
-        return alpha * self.__closes[-1] + (1 - alpha) * lastValue
+            lastValue = self.__closes[-1]
+        else:
+            alpha = 2 / (amount + 1)
+            lastValue = alpha * self.__closes[-1] + (1 - alpha) * lastValue
+        self.__lastValues.setdefault(type, lastValue)
+        if len(self.__closes) < amount:
+            return None
+        return lastValue
 
     def __calculateMA(self, type:movingAverage.MovingAverageType):
         averageData = movingAverage.MovingAverageData[type]
         if averageData[1] == movingAverage.MovingAverageMode.SMA:
             self.__averages[type] = self.__calculateSMA(averageData[0])
         elif averageData[1] == movingAverage.MovingAverageMode.EMA:
-            self.__averages[type] = self.__calculateEMA(self.__averages[type], averageData[0])
+            self.__averages[type] = self.__calculateEMA(type, averageData[0])
 
     def process(self, candle: candle.Candle):
         self.__closes.append(candle.close)
@@ -46,6 +52,7 @@ class MovingAverageController:
         return amount
 
     __averages:dict = {}
+    __lastValues:dict = {}
     __closes = []
     __maxAverageSize = 0
     __emaFactor = 8
