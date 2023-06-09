5b67c8767e25527d0719a91003fb05b559feb814
Maksim Isakov <maksim.isakov@mytona.com>
2023-06-09 22:49:53 +0400

    add strategy

--- source/main.py
+++ source/main.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
     # stats = pstats.Stats(prof).strip_dirs().sort_stats("cumtime")
     # stats.print_stats(10) # top 10 rows
 
-# to do add markable into cache
-# find bug with update
+# to do opened orders to top
+# to do parse data from orders
 # to do add websocket sync check by time
 # to do add root logs handler also for binance logs
 # to do request api system (from thread), every request return requestId, check for exceptions, limits
