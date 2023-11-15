e2888c15bcfd3e578473a391f79f9309a2002047
Maksim Isakov <maksim.isakov@mytona.com>
2023-11-16 02:21:37 +0400

    fix fee calculation

--- source/systems/feeController.py
+++ source/systems/feeController.py
@@ -1,13 +1,15 @@
 from systems import settingsController
 
-__feePercent = settingsController.getSetting('acceptableFeePercent')
+__feePercent = settingsController.getSetting('acceptableFeePercent') / 2 # open + close order
 __feePerShare = settingsController.getSetting('feePerShare')
+__atrStrategyFactor = settingsController.getSetting('atrStrategyFactor')
 
 def isFeeAcceptable(atr):
-	global __feePerShare,__feePercent
+	global __feePerShare,__feePercent,__atrStrategyFactor
 	if atr is None:
 		return False
 	
-	feePercent = __feePerShare / atr * 100
-	return feePercent <= __feePercent
+	atr = atr * __atrStrategyFactor
+	minimumAtr = 100 * __feePerShare / __feePercent
+	return atr >= minimumAtr
 	
