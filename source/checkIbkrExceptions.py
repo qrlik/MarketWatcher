85b35788a423c3e7c0d7c35c2d3560973d1c522f
Maksim Isakov <qrlikk@gmail.com>
2024-02-05 11:29:23 +0400

    add comments

--- source/checkIbkrExceptions.py
+++ source/checkIbkrExceptions.py
@@ -2,6 +2,12 @@ import api.third_party.ibkr as ibkr
 import api.stocks as stocks
 from utilities import utils
 
+# Get tickers list from yahoo
+# Filter by stockType,industry,category from ibkrExceptions.json
+# Get Final Data (ruleIds, data, exceptions, backets)
+# 
+# When request with info, dismiss expceptions, then dismiss new tickers (without ibkr info)
+
 def __updateIbkrExceptions():
     ibkr.runApp()
     ibkr.app.checkTickersList(stocks.getTickersList(False))
